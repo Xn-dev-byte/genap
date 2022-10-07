aa5d13e551edede1fe581c6c48695c173c1200d2
LuckySora007 <indra111.ida@gmail.com>
2022-10-07 10:59:07 +0700

    Add code and readme

---
+++ programs/helloworldusingFunc.cpp
@@ -0,0 +1,12 @@
+//create hello world using function
+#include <iostream>
+using namespace std;
+
+void helloWorld(){
+    cout<<"Hello World!";
+}
+
+int main(){
+    helloWorld();
+    return 0;
+}
